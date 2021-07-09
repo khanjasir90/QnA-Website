49e98edbf843b4ee196bec5e94bc856485ec5f06
Mohd Jasir Khan <khanmohdjasir@gmail.com>
2021-07-09 16:35:09 +0530

    added the Qr-Code functionality

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
     $('[data-toggle="popover"]').popover();   
 });
+
+
+$("#qr-code").on('show.bs.modal',function(event){
+    var qrcode = new QRCode(document.getElementById("qr-display"),{
+        text : window.location.href,
+        width : 128,
+        height : 128,
+        colorDark : "#5868bf",
+	    colorLight : "#ffffff",
+	    correctLevel : QRCode.CorrectLevel.H
+    })
+})
+
+$("#qr-code").on('hidden.bs.modal',function(event){
+    document.getElementById("qr-display").innerHTML = ""
+})
