29a2dae1195799c9d9c54c2b9f2fd3a456d7df3e
Mohd Jasir Khan <khanmohdjasir@gmail.com>
2021-07-11 22:10:01 +0530

    heroku commit

--- model/db.js
+++ model/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('./login')
 require('dotenv').config()
 const conn_url = process.env.CONN_URL
-mongoose.connect(conn_url,{useNewUrlParser:true},(err)=>{
+mongoose.connect("mongodb+srv://jasir-admin:cowin-db%23123@cluster0.5ofax.mongodb.net/QnA-Webapp?retryWrites=true&w=majority",{useNewUrlParser:true},(err)=>{
     if(!err) console.log('Connection created')
     else console.log('Error while connected to database'+err)
 })
