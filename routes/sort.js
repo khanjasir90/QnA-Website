7f3de81aaafc4e6142d90f8d10f12fba4744127e
Mohd Jasir Khan <khanmohdjasir@gmail.com>
2021-07-11 11:43:46 +0530

    added sort by votes and date functionality

--- routes/sort.js
+++ routes/sort.js
@@ -1,12 +1,42 @@
 const express = require('express')
+const Question = require('../model/question')
+const Answer = require('../model/answer')
 const router = express.Router()
 
-router.get('/sortAnswerByDate',(req,res)=>{
-    res.send('jasir')
+router.get('/sortByDate/:id',(req,res)=>{
+    Question.find({_id:req.params.id},(qerr,qobj)=>{
+        var dateSort = {date : -1}
+        Answer.find({question_id : req.params.id}).sort(dateSort).exec((aerr,aobj)=>{
+            if(aerr){
+                console.log(aerr)
+            }else{
+                res.render('answer',{
+                    username : req.session.username,
+                    question : qobj,
+                    answer : aobj,
+                    answerflag :  "",
+                    questionid : req.params._id
+                })
+            }
+        })
+    })
 })
 
-router.get('/sortAnswerByMostVote',(req,res)=>{
-    res.send('done')
+router.get('/sortByMostVotes/:id',(req,res)=>{
+    Question.find({_id:req.params.id},(qerr,qobj)=>{
+        var voteSort = {votes : -1}
+        Answer.find({question_id:req.params.id}).sort(voteSort).exec((aerr,aobj)=>{
+            res.render('answer',{
+                username : req.session.username,
+                question : qobj,
+                answer : aobj,
+                answerflag :  "",
+                questionid : req.params._id
+            })
+        })
+
+    })
 })
+
 
 module.exports = router
